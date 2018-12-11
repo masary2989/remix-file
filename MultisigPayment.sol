26f8164bdaef0143c2f8bed037028a6de0ac40fc
masary2989 <fxxkinspam@gmail.com>
2018-12-11 19:30:15 +0900

    Update MultisigPayment.sol
    confirmselling動いたけど、ownerとかmsg.senderとか意図通りの記述になってないので、コメント見ながら直す

--- MultisigPayment.sol
+++ MultisigPayment.sol
@@ -17,20 +17,19 @@ contract MultisigPayment is Ownable{
     }
     
     struct Transaction {
-        uint depositId;
         uint amount;
-        // bytes data;
+        bool confirmed;
         bool executed;
     }
     
-    mapping (uint => Deposit) private deposits;
-    mapping (uint => Transaction) private transactions;
-    mapping (address => uint) private userToDeposit;
+    // @dev mappingは全てあとでprivateへ変更
+    mapping (address => Deposit) public deposits;
+    mapping (uint => Transaction) public transactions;
+    mapping (address => uint[]) public userToTransactions;
     mapping (uint => mapping (address => bool)) public confirmations;
     
     uint constant public MAX_OWNER_COUNT = 5;
     uint required;
-    uint depositCount;
     uint transactionCount;
     
     // modifiers
@@ -45,7 +44,26 @@ contract MultisigPayment is Ownable{
     }
     
     modifier onlyDepositedUser() {
-        require(userToDeposit[msg.sender] != 0);
+        require(deposits[msg.sender].depositAmount != 0);
+        _;
+    }
+    // @dev 今支払う額も入れる
+    // @dev msg.senderじゃなくて、送り先（これのmsg.senderはownerになる）
+    modifier validBalance() {
+        uint usedAmount;
+        if (userToTransactions[msg.sender].length > 0) {
+            uint[] storage transactionIds = userToTransactions[msg.sender];
+            for (uint i=0; i<transactionIds.length; i.add(1)) {
+                if (transactions[transactionIds[i]].executed
+                    && transactions[transactionIds[i]].confirmed) {
+                    usedAmount.add(transactions[transactionIds[i]].amount);
+                } else if (!transactions[transactionIds[i]].executed) {
+                    usedAmount.add(transactions[transactionIds[i]].amount);
+                }
+            
+            }
+            require(deposits[msg.sender].depositAmount.sub(usedAmount) > 0);
+        }
         _;
     }
 
@@ -55,9 +73,9 @@ contract MultisigPayment is Ownable{
         public
     {
         required = 1;
-        depositCount = 0;
         transactionCount = 0;
         daiContract = ERC20Interface(_daiInterfaceAddress);
+        
     }
     
     function setDaiContractAddress (address _contractAddress) external onlyOwner {
@@ -66,28 +84,46 @@ contract MultisigPayment is Ownable{
     
     //@dev sudenidepositsiteruuserniha,sokonidepositsuru.
     function () payable public {
-        deposits[depositCount] = Deposit(msg.value, msg.sender);
-        userToDeposit[msg.sender] = depositCount;
-        depositCount = depositCount.add(1);
+        deposits[msg.sender] = Deposit(msg.value, msg.sender);
     }
     
     function depositDai (uint _amount) public {
-        require(daiContract.transfer(address(this), _amount));
+        require(daiContract.transferFrom(msg.sender, address(this), _amount));
+        deposits[msg.sender] = Deposit(_amount, msg.sender);
     }
     
-    function makeTransaction (uint _depositId, uint _amount) private {
+    //@dev これのmsg.senderはownerになる
+    function _makeTransaction (uint _amount) internal returns (bool success) {
+        userToTransactions[msg.sender].push(transactionCount);
         transactions[transactionCount] = Transaction({
-            depositId: _depositId,
             amount: _amount,
-            //data: _data,
+            confirmed: false,
             executed: false
         });
+        return (true);
     }
-    // @dev msg.sender? owner あとで直す
-    function confirmSelling (uint _depositId, uint _amount, address _userAddress) public onlyDepositedUser{
-        confirmations[transactionCount][msg.sender] = false;
+    // @dev 一個目のconfirmはmsg.sender? owner あとで直す
+    function confirmSelling (uint _depositId, uint _amount, address _userAddress) public onlyOwner validBalance {
+        confirmations[transactionCount][owner()] = true;
         confirmations[transactionCount][_userAddress] = false;
-        makeTransaction(_depositId, _amount);
+        require(_makeTransaction(_amount));
         transactionCount = transactionCount.add(1);
     }
+    
+    function userDeposit () public view returns (uint, address) {
+        uint depositAmount = deposits[msg.sender].depositAmount;
+        address userAddress = deposits[msg.sender].userAddress;
+        return (depositAmount, userAddress);
+    }
+    
+    function userTransactions () public view returns (uint[]) {
+        return (userToTransactions[msg.sender]);
+    }
+    
+    function userTransaction (uint _transactionId) public view returns (uint, bool, bool) {
+        uint amount = transactions[_transactionId].amount;
+        bool confirmed = transactions[_transactionId].confirmed;
+        bool executed = transactions[_transactionId].executed;
+        return (amount, confirmed, executed);
+    } 
 }
